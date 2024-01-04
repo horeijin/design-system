7d44497047fc90625217b531ac8171c0399f86ad
myMac <dgbuzzer@gmail.com>
2024-01-04 15:07:18 +0900

    feature2-2/common config

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,38 +1,8 @@
-import esbuild from 'esbuild';
+import run from '@frontproject/esbuild-config';
+import pkg from './package.json' assert { type: 'json' };
 
-const dev = process.argv.includes('--dev');
-const minify = !dev;
-
-const watch = process.argv.includes('--watch');
-
-
-const baseConfig = {
-    entryPoints: ['src/index.ts'],
-    bundle: true,
-    minify,
-    sourcemap: true,
-    outdir: 'dist',
-    target: 'es2019',
-    watch,
-}
-
-//parallel ECMA Script Module & CJS build
-Promise.all([
-    esbuild.build({
-        ...baseConfig,
-        format: 'esm',
-    }),
-    esbuild.build({
-        ...baseConfig,
-        format: 'cjs',
-        outExtension:{
-            '.js': '.cjs'
-        }
-    })
-]).catch(()=>{
-    console.error('Build Failed...')
-    process.exit(1)
-});
-
+run({
+    pkg,
+})
 
 
