7088c44d69bc6d26c5eaf5b7edc6cf3896b8d3db
myMac <dgbuzzer@gmail.com>
2024-03-29 09:40:21 +0900

    feature2-13/toast provider

---
+++ services/storybook/stories/ReactComponents/Toast/Toast.stories.tsx
@@ -0,0 +1,40 @@
+import "@frontproject/react-components-button/style.css";
+import { Button } from '@frontproject/react-components-button';
+import React from "react";
+
+import { ToastProvider, useToast } from '@frontproject/react-components-toast';
+import "@frontproject/react-components-toast/style.css";
+
+export default {
+  title: "React Components/Toast",
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+const Example = () => {
+  const { toast } = useToast();
+
+  return (
+    <Button
+      onClick={() =>
+        toast({
+          payload: {
+            message: "Hello, World!",
+          },
+        })
+      }
+    >
+      Toast Button
+    </Button>
+  );
+}
+
+export const ButtonStory = {
+  render: () => (
+    <ToastProvider>
+      <Example />
+    </ToastProvider>
+  ),
+};
